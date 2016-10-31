8693a890c5ba2e9b0030b3e0840dc7f212f407fc
Matthew McEachen <matthew+github@mceachen.org>
2016-10-31 07:27:09 -0700

    module/namespace isn't needed

--- src/exiftool.ts
+++ src/exiftool.ts
@@ -6,111 +6,109 @@ import { Task } from './task'
 export { Tags } from './tags'
 export { ExifDate, ExifTime, ExifDateTime, ExifTimeZoneOffset } from './datetime'
 
-export module ExifTool {
-  export interface ExifToolAPI {
-    /**
-     * @return a promise holding the version number of the vendored ExifTool
-     */
-    version(): Promise<string>
-    /**
-     * @return a Promise holding the metadata tags found in `file`.
-     */
-    read(file: string): Promise<Tags>
-
-    /**
-     * Request graceful shut down of any running ExifTool child processes.
-     *
-     * This may need to be called in `after` or `finally` clauses in tests
-     * or scripts for them to exit cleanly.
-     */
-    end(): void
-  }
-
-  export interface Logger {
-    info(msg: string): void
-    warn(msg: string): void
-    error(msg: string): void
-  }
-
+export interface ExifToolAPI {
   /**
-   * Assign your custom logger to this instance. 
+   * @return a promise holding the version number of the vendored ExifTool
    */
-  export let logger = console
-
+  version(): Promise<string>
   /**
-   * This is the version of the `exiftool-vendored` npm module.
-   * The package.json value is made to match this value by `npm run update`. 
+   * @return a Promise holding the metadata tags found in `file`.
    */
-  export const ExifToolVendoredVersion = '0.3.0'
+  read(file: string): Promise<Tags>
 
   /**
-   * Manages delegating calls to a vendored running instance of ExifTool.
+   * Request graceful shut down of any running ExifTool child processes.
    *
-   * Instantiation is expensive: use the exported singleton instance of this class, `exiftool`.
+   * This may need to be called in `after` or `finally` clauses in tests
+   * or scripts for them to exit cleanly.
    */
-  export class ExifTool implements ExifToolAPI {
-    private _procs: ExifToolProcess[] = []
-    private _tasks: Task<any>[] = []
+  end(): void
+}
 
-    /**
-     * @param maxProcs the maximum number of ExifTool child processes to spawn when load merits
-     */
-    constructor(
-      readonly maxProcs: number = 1
-    ) { } // tslint:disable-line
+export interface Logger {
+  info(msg: string): void
+  warn(msg: string): void
+  error(msg: string): void
+}
 
-    /**
-     * @return a Promise to the vendored ExifTool's version 
-     */
-    version(): Promise<string> {
-      return this.enqueueTask(new VersionTask()).promise
-    }
+/**
+ * Assign your custom logger to this instance. 
+ */
+export let logger = console
 
-    /**
-     * @return a Promise holding the metadata tags found in `file`.
-     */
-    read(file: string): Promise<Tags> {
-      return this.enqueueTask(TagsTask.for(file)).promise
-    }
+/**
+ * This is the version of the `exiftool-vendored` npm module.
+ * The package.json value is made to match this value by `npm run update`. 
+ */
+export const ExifToolVendoredVersion = '0.3.0'
 
-    /**
-     * Request graceful shut down of any running ExifTool child processes.
-     *
-     * This may need to be called in `after` or `finally` clauses in tests
-     * or scripts for them to exit cleanly.
-     */
-    end(): Promise<void> {
-      this._procs.forEach(p => p.end())
-      return Promise.all(this._procs.map(p => p.closedPromise))
-    }
+/**
+ * Manages delegating calls to a vendored running instance of ExifTool.
+ *
+ * Instantiation is expensive: use the exported singleton instance of this class, `exiftool`.
+ */
+export class ExifTool implements ExifToolAPI {
+  private _procs: ExifToolProcess[] = []
+  private _tasks: Task<any>[] = []
 
-    enqueueTask<T>(task: Task<T>): Task<T> {
-      this._tasks.push(task)
-      this.workIfIdle()
-      return task
-    }
+  /**
+   * @param maxProcs the maximum number of ExifTool child processes to spawn when load merits
+   */
+  constructor(
+    readonly maxProcs: number = 1
+  ) { } // tslint:disable-line
 
-    private dequeueTask(): Task<any> | undefined {
-      return this._tasks.shift()
-    }
-
-    private procs(): ExifToolProcess[] {
-      return this._procs = this._procs.filter(p => !p.ended)
-    }
-
-    private workIfIdle(): void {
-      const idle = this._procs.find(p => !p.ended && p.idle)
-      if (idle) {
-        idle.workIfIdle()
-      } else if (this.procs().length < this.maxProcs) {
-        this._procs.push(new ExifToolProcess(() => this.dequeueTask()))
-      }
-    }
+  /**
+   * @return a Promise to the vendored ExifTool's version 
+   */
+  version(): Promise<string> {
+    return this.enqueueTask(new VersionTask()).promise
   }
 
   /**
-   * Use this singleton rather than instantiating new ExifTool instances
-   * in order to leverage a single running ExifTool process.
+   * @return a Promise holding the metadata tags found in `file`.
    */
-  export const exiftool: ExifToolAPI = new ExifTool()
+  read(file: string): Promise<Tags> {
+    return this.enqueueTask(TagsTask.for(file)).promise
+  }
+
+  /**
+   * Request graceful shut down of any running ExifTool child processes.
+   *
+   * This may need to be called in `after` or `finally` clauses in tests
+   * or scripts for them to exit cleanly.
+   */
+  end(): Promise<void> {
+    this._procs.forEach(p => p.end())
+    return Promise.all(this._procs.map(p => p.closedPromise))
+  }
+
+  enqueueTask<T>(task: Task<T>): Task<T> {
+    this._tasks.push(task)
+    this.workIfIdle()
+    return task
+  }
+
+  private dequeueTask(): Task<any> | undefined {
+    return this._tasks.shift()
+  }
+
+  private procs(): ExifToolProcess[] {
+    return this._procs = this._procs.filter(p => !p.ended)
+  }
+
+  private workIfIdle(): void {
+    const idle = this._procs.find(p => !p.ended && p.idle)
+    if (idle) {
+      idle.workIfIdle()
+    } else if (this.procs().length < this.maxProcs) {
+      this._procs.push(new ExifToolProcess(() => this.dequeueTask()))
+    }
+  }
 }
+
+/**
+ * Use this singleton rather than instantiating new ExifTool instances
+ * in order to leverage a single running ExifTool process.
+ */
+export const exiftool: ExifToolAPI = new ExifTool()
