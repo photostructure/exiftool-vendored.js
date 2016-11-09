14ae0008da9feb8ff0f932be9f980d8da2e5b0c1
Matthew McEachen <matthew+github@mceachen.org>
2016-11-08 16:02:50 -0800

    semicolons for codacy

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,30 +1,30 @@
-const globule = require('globule')
-const exif = require('exiftool')
-const process = require('process')
+const globule = require('globule');
+const exif = require('exiftool');
+const process = require('process');
 
-const root = process.argv[2]
-const files = globule.find(`${root}/**/*.jpg`)
+const root = process.argv[2];
+const files = globule.find(`${root}/**/*.jpg`);
 
 if (files.length === 0) {
-  console.error(`No files found in ${root}`)
-  process.exit(1)
+  console.error(`No files found in ${root}`);
+  process.exit(1);
 } else {
-  console.log("Reading " + files.length + " files...")
+  console.log("Reading " + files.length + " files...");
 }
 
 function prom(file) {
   return new Promise((resolve, reject) => {
     exif.metadata(file, (err, metadata) => {
-      process.stdout.write('.')
-      if (err) reject(err)
-      else resolve() // throw away
-    })
-  })
+      process.stdout.write('.');
+      if (err) reject(err);
+      else resolve(); // throw away
+    });
+  });
 }
 
-const start = Date.now()
+const start = Date.now();
 
 Promise.all(files.map(f => prom(f))).then(() => {
-    const elapsedMs = Date.now() - start
-    console.log(`Parsing took ${elapsedMs}ms (${(elapsedMs / files.length).toFixed(1)}ms / file)`)
-})
+  const elapsedMs = Date.now() - start;
+  console.log(`Parsing took ${elapsedMs}ms (${(elapsedMs / files.length).toFixed(1)}ms / file)`);
+});
