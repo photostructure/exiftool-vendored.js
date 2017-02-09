0cf1e73790ac074ec5225733c43bd41b62e2bc40
Matthew McEachen <matthew+github@mceachen.org>
2017-02-08 21:15:41 -0800

    upgrade to 10.41

--- src/exiftool.spec.ts
+++ src/exiftool.spec.ts
@@ -14,7 +14,7 @@ describe("ExifTool", () => {
   }
 
   it("returns the correct version", () => {
-    return expect(exiftool.version()).to.become("10.40")
+    return expect(exiftool.version()).to.become("10.41")
   })
 
   it("returns expected results for a given file", () => {
