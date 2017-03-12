6c91e1fd439d33d02ee0372658283661aa3306ca
Matthew McEachen <matthew+github@mceachen.org>
2017-03-12 16:41:18 -0700

    v2.9.0

--- src/exiftool.spec.ts
+++ src/exiftool.spec.ts
@@ -14,7 +14,7 @@ describe("ExifTool", () => {
   }
 
   it("returns the correct version", () => {
-    return expect(exiftool.version()).to.become("10.44")
+    return expect(exiftool.version()).to.become("10.46")
   })
 
   it("returns expected results for a given file", () => {
