3d0591f736399b52fce97a5930656a1cddf6ba3b
amokrushin <anton@mokr.org>
2017-05-25 00:02:10 +0500

    exiftool-path script

--- postinstall.js
+++
@@ -1,8 +0,0 @@
-var process = require("process")
-var isWin32 = process.platform === "win32"
-
-try {
-  require("exiftool-vendored." + (isWin32 ? "exe" : "pl"))
-} catch (err) {
-  throw new Error("Vendored ExifTool does not installed")
-}
