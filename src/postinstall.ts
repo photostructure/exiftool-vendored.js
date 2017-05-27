f1d30f1a44423caeee815126a132a18664cac5e1
Matthew McEachen <matthew+github@mceachen.org>
2017-05-27 00:21:42 -0700

    bah humbug postinstall
    there's no way to discriminate in a script lifecycle event if the "postinstall" is from local development (which needs tsc) or installation via a remotely fetched tarball (which should not need tsc)

--- src/postinstall.ts
+++
@@ -1,9 +0,0 @@
-// By requiring ExifTool, we'll be verifying that the vendored ExifTool
-// is also properly installed.
-
-try {
-  require("./ExifTool")
-} catch (err) {
-  console.error("Installation failed: " + err)
-  require("process").exit(1)
-}
