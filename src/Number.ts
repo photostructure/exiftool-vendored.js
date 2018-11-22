d4a06facd6b704bc33810aa6e0510349e45c644a
Matthew McEachen <matthew+github@mceachen.org>
2018-11-21 20:38:36 -0800

    Simplify float parsing.
    Parsing only allows strings that start with a number now.

--- src/Number.ts
+++ src/Number.ts
@@ -8,15 +8,11 @@ export function toI(n: any): Maybe<number> {
   return map(toF(n), f => Math.round(f))
 }
 
-const numRe = /([+-]?[0-9]+(?:\.(?:[0-9]*))?)/
-
 export function toF(n: any): Maybe<number> {
   if (n == null) return
   if (isNumber(n)) return n
-  const m = numRe.exec(n)
-  if (m == null) return
   try {
-    return parseFloat(m[1])
+    return parseFloat(String(n))
   } catch {
     return undefined
   }
