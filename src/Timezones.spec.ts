697f64fdf378b32cc095e8e3c6008bf60490aab7
Matthew McEachen <matthew@photostructure.com>
2019-07-21 17:56:28 -0700

    prettier

--- src/Timezones.spec.ts
+++ src/Timezones.spec.ts
@@ -3,20 +3,23 @@ import { extractOffset } from "./Timezones"
 
 describe("Timezones", () => {
   describe("extractOffsetMinutes", () => {
-    const arr = [
-      { tz: "7", exp: "UTC+7" },
-      { tz: "3:30", exp: "UTC+03:30" },
-    ]
+    const arr = [{ tz: "7", exp: "UTC+7" }, { tz: "3:30", exp: "UTC+03:30" }]
     ;[
       { tz: "", exp: undefined },
       { tz: "garbage", exp: undefined },
       { tz: "09:00", exp: "UTC+9" },
       { tz: "America/Los_Angeles", exp: "America/Los_Angeles" },
       ...arr,
-      ...arr.map(({tz, exp}) => ({tz: "+" + tz, exp})),
-      ...arr.map(({tz, exp}) => ({tz: "UTC+" + tz, exp})),
-      ...arr.map(({tz, exp}) => ({tz: "-" + tz, exp: exp.replace("+", "-")})),
-      ...arr.map(({tz, exp}) => ({tz: "UTC-" + tz, exp: exp.replace("+", "-")}))
+      ...arr.map(({ tz, exp }) => ({ tz: "+" + tz, exp })),
+      ...arr.map(({ tz, exp }) => ({ tz: "UTC+" + tz, exp })),
+      ...arr.map(({ tz, exp }) => ({
+        tz: "-" + tz,
+        exp: exp.replace("+", "-")
+      })),
+      ...arr.map(({ tz, exp }) => ({
+        tz: "UTC-" + tz,
+        exp: exp.replace("+", "-")
+      }))
     ].forEach(({ tz, exp }) =>
       it(`("${tz}") => ${exp}`, () => {
         expect(extractOffset(tz)).to.eql(exp)
