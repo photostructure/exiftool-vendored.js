3bff755fbe81d62c369cf5ac4a8a74abc4452752
Matthew McEachen <matthew@photostructure.com>
2019-10-12 10:00:25 -0700

    allow exiftool to throw errors and still give a ms/file benchmark

--- benchmark/index.js
+++ benchmark/index.js
@@ -1,35 +1,44 @@
-const globule = require("globule");
-const exif = require("exiftool");
-const process = require("process");
+const globule = require("globule")
+const exif = require("exiftool")
+const process = require("process")
 
-const root = process.env.root || ".";
-const files = globule.find(`${root}/**/*.jpg`);
+const dir = process.argv[2]
+const files = globule.find(`${dir}/**/*.jpg`).slice(0, 1000)
 
 if (files.length === 0) {
-  console.error(`No files found in ${root}`);
-  process.exit(1);
+  console.error(`Please provide a directory with example images.`)
+  console.error(`Usage: node ./index.js IMAGE_DIRECTORY`)
+  process.exit(1)
 } else {
-  console.log("Reading " + files.length + " files...");
+  console.log("Reading " + files.length + " files...")
 }
+
+const errors = []
 
 function prom(file) {
   return new Promise((resolve, reject) => {
-    exif.metadata(file, (err, metadata) => {
-      process.stdout.write(".");
-      if (err) reject(err);
-      else resolve(); // throw away
-    });
-  });
+    try {
+      exif.metadata(file, (err, metadata) => {
+        if (err) errors.push("failed to read " + file + ": " + err)
+        resolve() // throw away
+      })
+    } catch (err) {
+      errors.push("error raised from " + file + ": " + err)
+      resolve()
+    }
+  })
 }
 
-console.log("Starting...");
-const start = Date.now();
+const start = Date.now()
 
 Promise.all(files.map(f => prom(f))).then(() => {
-  const elapsedMs = Date.now() - start;
+  const elapsedMs = Date.now() - start
   console.log(
     `exiftool: Parsing took ${elapsedMs}ms (${(
       elapsedMs / files.length
     ).toFixed(1)}ms / file)`
-  );
-});
+  )
+  if (errors.length > 0) {
+    console.log("exiftool had errors:", errors)
+  }
+})
