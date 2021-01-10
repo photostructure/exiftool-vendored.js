52dffde2de0257b353c9c9a77bbf40c1a5f16430
Matthew McEachen <matthew@photostructure.com>
2021-01-10 15:23:54 -0800

    fight new typedoc changes

--- .typedoc.js
+++ .typedoc.js
@@ -5,9 +5,11 @@ module.exports = {
   includes: "./src",
   gitRevision: "master", // < prevents docs from changing after every commit
   exclude: ["**/update/*", "**/*test*", "**/*spec*"],
-  mode: "file", 
+  // mode: "file",
   excludePrivate: true,
-  excludeProtected: true,
-  excludeExternals: true,
-  excludeNotExported: true
+  // excludeProtected: true,
+  // excludeExternals: true,
+  entryPoints: ["./src/ExifTool.ts", "./src/Tags.ts", "./src/BinaryExtractionTask.ts", "./src/ExifDate.ts", "./src/ExifDateTime.ts", "./src/ReadTask.ts", "./src/WriteTask.ts"],
+  // entryPoints: ["./src"]
+  // excludeNotExported: true
 }
