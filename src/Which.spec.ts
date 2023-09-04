8b8028353407dd3acb1f641084e503f6a7d6f528
Matthew McEachen <matthew@photostructure.com>
2023-09-03 22:03:41 -0700

    add doc

--- src/Which.spec.ts
+++ src/Which.spec.ts
@@ -6,6 +6,8 @@ if (!isWin32()) {
   describe("Which", () => {
     it("finds perl", async () => {
       const act = await which("perl")
+      // macOS is `/usr/local/bin/perl`. Most linux distros will use
+      // `/usr/bin/perl`, but could be `/bin/perl` and still be valid.
       expect(act).to.match(/^(\/usr(\/local)?)?\/bin\/perl$/)
     })
     it("finds node", async () => {
