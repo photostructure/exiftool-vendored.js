7651ff0f110700a2f49d178b64f9437e4c81f883
Matthew McEachen <matthew@photostructure.com>
2024-07-20 12:37:22 -0700

    fix ImageDataHash jsdocs

--- src/ImageDataHashTag.ts
+++ src/ImageDataHashTag.ts
@@ -1,9 +1,10 @@
 export interface ImageDataHashTag {
   /**
-   * This is calculated by ExifTool to be the SHA256 hash of the image data
-   * (ignoring metadata). This tag is only included if the default
-   * `ExifToolOptions.imageHashType` value is overridden with a valid string
-   * value.
+   * This is calculated by ExifTool to be the MD5, SHA256, or SHA512 hash of
+   * just the image data, excluding metadata. 
+   *
+   * This tag is only included if the default `ExifToolOptions.imageHashType`
+   * value is overridden with a valid string value.
    *
    * @see ExifToolOptions.imageHashType
    */
