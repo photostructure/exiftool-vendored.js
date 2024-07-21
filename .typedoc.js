a826360fd3a27e300927ea24a01814fce0043dec
Matthew McEachen <matthew@photostructure.com>
2024-07-20 20:05:43 -0700

    yarn docs

--- .typedoc.js
+++ .typedoc.js
@@ -2,7 +2,6 @@ module.exports = {
   name: "exiftool-vendored",
   out: "./docs/",
   readme: "./README.md",
-  includes: "./src",
   gitRevision: "main", // < prevents docs from changing after every commit
   exclude: ["**/update/*", "**/*test*", "**/*spec*"],
   excludePrivate: true,
