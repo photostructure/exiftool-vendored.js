206397e4bd8fc6dbeeade1f5bf02676acb5970e9
Matthew McEachen <matthew@photostructure.com>
2024-11-04 10:53:22 -0800

    Refactor times function to use Array.from for improved readability

--- src/Times.ts
+++ src/Times.ts
@@ -1,5 +1,9 @@
+/**
+ * Generates an array by running a function n times
+ * @param n The number of times to run the function
+ * @param fn The function to generate each element
+ * @returns An array containing the results
+ */
 export function times<T>(n: number, f: (idx: number) => T): T[] {
-  return Array(n)
-    .fill(undefined)
-    .map((_, i) => f(i))
+  return Array.from({ length: n }, (_, i) => f(i))
 }
