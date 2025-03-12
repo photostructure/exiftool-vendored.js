db3c5cc170b080e64c791fa37634faed25151727
Matthew McEachen <matthew@photostructure.com>
2025-03-12 10:29:44 -0700

    feat(DefaultMaxProcs): improve CPU core calculation for DefaultMaxProcs with support for availableParallelism

--- src/DefaultMaxProcs.ts
+++ src/DefaultMaxProcs.ts
@@ -1,3 +1,16 @@
 import * as _os from "node:os";
+import { isFunction } from "./Object";
 
-export const DefaultMaxProcs = Math.max(1, Math.floor(_os.cpus().length / 4));
+function maxCpus() {
+  // https://nodejs.org/api/os.html#osavailableparallelism was added in node
+  // 18.14, but people may still be running archaic versions of node.
+
+  return Math.max(
+    1,
+    isFunction(_os.availableParallelism)
+      ? _os.availableParallelism()
+      : _os.cpus().length,
+  );
+}
+
+export const DefaultMaxProcs = Math.ceil(maxCpus() / 4);
